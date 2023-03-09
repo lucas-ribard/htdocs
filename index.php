8cfcc9b53e2cba208e248c2c32902ab4ba02008a
lucas-ribard <lucas.ribard@laplateforme.io>
2023-03-09 15:55:43 +0100

    first

---
+++ index.php
@@ -0,0 +1,42 @@
+<head>
+    <link href="index.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <section id="main">
+
+
+        <div class="liste">
+            <h2>Projets :</h2><br>
+            <a href="http://localhost/module-connexion/">module de connexion</a><br><br>
+            <a href="http://localhost/Livre-or/">livre d'or</a><br><br>
+            <a href="http://localhost/reservation-salles/index.php">reservation</a><br><br> 
+            <a href="http://localhost/classes-php/index.php">Classes-php / Module de Connexion </a><br><br>
+            <a href="http://localhost/memory/index.php">Memory</a><br><br>
+            <a href="http://localhost/oclock/index.php">O'clock</a><br><br>
+            <a href="http://localhost/livre-or-js/">livre d'or JS</a><br><br>
+            <a href="http://localhost/ToDoList/">To Do List</a><br><br>
+            <a href="http://localhost/clicker/">Cliker</a><br><br>
+            <a href="http://localhost/Blog-js/">Blog</a><br><br>
+        </div>
+
+        <div class="liste">
+            <h2>Runtracks :</h2><br>
+            <a href="http://localhost/runtrack2/">Runtrack 2</a><br><br>
+            <a href="http://localhost/Runtrack3/">Runtrack 3</a><br><br>
+        </div>
+
+        <div class="liste">
+        <h2>Perso :</h2><br>
+        <a href="http://localhost/Vitrine/index.php">Portfolio</a><br><br>
+        <a href="http://localhost/Game/index.html">Space-Invader</a><br><br>
+       
+        </div>
+
+
+    </section>
+</body>
+
+
+
